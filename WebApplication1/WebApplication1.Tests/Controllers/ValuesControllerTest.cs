68767249582434f0d749c0d082e3b707155a8fba
Diana Buras <burasdiana@hotmail.com>
2019-01-29 13:13:08 +0100

    [add] test testmethod

--- WebApplication1/WebApplication1.Tests/Controllers/ValuesControllerTest.cs
+++ WebApplication1/WebApplication1.Tests/Controllers/ValuesControllerTest.cs
@@ -100,6 +100,22 @@ namespace WebApplication1.Tests.Controllers
         }
 
         [TestMethod]
+        public void Calculate_ShouldDetectInvalidInputs2()
+        {
+            // Arrange
+            ValuesController controller = new ValuesController();
+            int a = 1 / 2;
+            int b = 10;
+            string op = "%";
+            // Act
+            int result = controller.Calculate(a, b, op);
+            int expected = 0;
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
         public void Put()
         {
             // Arrange
